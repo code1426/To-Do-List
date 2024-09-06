67b1f4e836b18bc224113d7c4efaf27e8c913fce
code1426 <gmver27@gmail.com>
2024-09-06 08:55:11 +0800

    reconstructed main.ts

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,77 +3,83 @@ const listContainer = document.querySelector<HTMLUListElement>("#list-container"
 const form = document.querySelector<HTMLFormElement>(".form")
 let ToDoArray: ToDo[] = JSON.parse(localStorage.getItem('todos') || '[]');
 
-interface ToDo {
+interface ToDo { 
   readonly id: number;
   title: string;
   isCompleted: boolean;
 }
 
-const createToDoItem = (toDo: ToDo) => {
-  const toDoItem = document.createElement("li")
-
-  //checkbox
+const checkboxComponent = ({ id, isCompleted }: ToDo, text: HTMLParagraphElement): HTMLInputElement => {
   const checkbox = document.createElement("input")
   checkbox.setAttribute("type", "checkbox")
-  checkbox.checked = toDo.isCompleted
+  checkbox.checked = isCompleted
+  
   checkbox.onchange = () => {
-    const item = ToDoArray.find(item => item.id === toDo.id);
+    const item = ToDoArray.find(item => item.id === id);
     if (item) {
       item.isCompleted = checkbox.checked;
       localStorage.setItem('todos', JSON.stringify(ToDoArray));
     }
-    title.className = checkbox.checked ? "text-cut" : "";
+    text.className = checkbox.checked ? "text-cut" : "";
   }
+  return checkbox
+}
 
-  //task title
-  const title = document.createElement("p");
-  title.className = toDo.isCompleted ? "text-cut" : "";
-  title.innerHTML = `${toDo.title}`;
-  
-  //delete button
+const textComponent = ({ isCompleted, title }: ToDo): HTMLParagraphElement => {
+  const text = document.createElement("p");
+  text.className = isCompleted ? "text-cut" : "";
+  text.innerHTML = `${title}`;
+  return text
+}
+
+const deleteBtnComponent = ({ id }: ToDo): HTMLSpanElement => {
   const deleteBtn = document.createElement("span")
   deleteBtn.innerHTML = "\u00d7"
   deleteBtn.onclick = () => {
-    deleteToDo(toDo.id)
+    deleteToDo(id)
   }
+  return deleteBtn
+}
 
-  //append elements
-  toDoItem.append(checkbox, title, deleteBtn)
-  listContainer?.appendChild(toDoItem) 
+const createToDoItem = (toDo: ToDo): void => {
+  const toDoItem = document.createElement("li")
+  const text = textComponent(toDo)
+  const checkbox = checkboxComponent(toDo, text)
+  const deleteBtn = deleteBtnComponent(toDo)
+
+  toDoItem.append(checkbox, text, deleteBtn)
+  listContainer?.appendChild<HTMLLIElement>(toDoItem) 
   inputBox!.value = ""
 }
 
-
-const deleteToDo = (id: number) => {
+const deleteToDo = (id: number): void => {
   ToDoArray = ToDoArray.filter(item => item.id !== id);
   localStorage.setItem('todos', JSON.stringify(ToDoArray));
   renderToDo();
 }
 
-
-const renderToDo = () => {
+const renderToDo = (): void => {
   listContainer!.innerHTML = ToDoArray.length ? "" : "<p class='no-task'>No To Do Yet!</p>";
-  ToDoArray.forEach(item => {
-    createToDoItem(item);
+  ToDoArray.forEach(toDo => {
+    createToDoItem(toDo);
   })
 }
 
-
-const handleSubmit = (e: Event) => {
-  e.preventDefault()
-  if (inputBox?.value === "" || inputBox?.value === null) return
+const handleSubmit = (event: Event): void => {
+  event.preventDefault()
+  const task = inputBox?.value.trim()
+  if (task === "" || task === null) return
 
   const newTodo: ToDo = {
     id: Date.now(),
-    title: inputBox!.value,
+    title: inputBox!.value.trim(),
     isCompleted: false
   }
 
   ToDoArray.push(newTodo);
   localStorage.setItem('todos', JSON.stringify(ToDoArray));
-
   renderToDo()
 }
 
-form?.addEventListener("submit",(e) => handleSubmit(e))
+form?.addEventListener("submit", (e) => handleSubmit(e))
 renderToDo()
